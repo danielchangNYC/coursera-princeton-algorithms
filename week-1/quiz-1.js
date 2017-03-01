96978ee00d6806aebe66812d4b9881177b6f1c6b
Daniel Chang <daniel.chang85@gmail.com>
2017-02-28 23:10:51 -0500

    Write FriendshipFinder data structure.

--- week-1/quiz-1.js
+++ week-1/quiz-1.js
@@ -10,6 +10,8 @@
 */
 
 /*
+  THOUGHTS:
+
   N = # of members in social network
   m = # of timestamps of when a friendship was made
 
@@ -18,3 +20,56 @@
   This is a dynamic connectivity problem. It involves connected components.
   Prompt asks for earliest point where all nodes are in one component.
 */
+
+// VANILLA.JS
+
+var FriendshipFinder = function(population) {
+  // this.population = Array.apply(null, Array(population)).map(function (_, i) {return i;});
+  this.population = [];
+  this.groupSizes = [];
+  this.groups = population;
+
+  for (var i = 0; i < population; i++) {
+    this.population[i] = i;
+  }
+
+  for (var i = 0; i < population; i++) {
+    this.groupSizes[i] = 1;
+  }
+
+  return this;
+};
+
+FriendshipFinder.prototype.allFriends = function() {
+  return this.groups = 1;
+};
+
+FriendshipFinder.prototype.rootFor = function(person) {
+  var current = person;
+  while (current !== this.population[current]) {
+    current = this.population[current];
+  }
+  return current;
+};
+
+FriendshipFinder.prototype.makeFriendship = function(person1, person2) {
+  var person1Root = this.rootFor(person1);
+  var person2Root = this.rootFor(person2);
+
+  if (person1Root === person2Root) {
+    return;
+  }
+
+  var person1GroupSize = this.groupSizes[person1Root];
+  var person2GroupSize = this.groupSizes[person2Root];
+
+  if (person1GroupSize > person2GroupSize) {
+    this.population[person2Root] = person1Root;
+    this.groupSizes[person1Root] += this.groupSizes[person2Root];
+  } else {
+    this.population[person1Root] = person2Root;
+    this.groupSizes[person2Root] += this.groupSizes[person1Root];
+  }
+
+  this.groups--;
+};
