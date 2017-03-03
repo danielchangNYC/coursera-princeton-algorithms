6009b0efc7e90333ecdea7f7915221175f85274c
Daniel Chang <daniel.chang85@gmail.com>
2017-03-03 15:12:32 -0500

    Finished recursive quick sort

--- week-3/quick_sort.rb
+++ week-3/quick_sort.rb
@@ -10,11 +10,11 @@ STRATEGY:
     - STRATEGY:
       - use first element as anchor a[lo]
       - two idx trackers, i and j.
-      - until i and j cross (that is, i > j)
+      - until i and j cross (that is, i >= j)
         - i++ as long as a[i] < a[lo]
         - j++ as long as a[j] > a[lo]
         - when both stop, SWAP THEM
-      - at this point, swap a[lo] with a[i]
+      - at this point, swap a[lo] with a[j]
   - Sort each piece recursively
 =end
 
@@ -32,36 +32,31 @@ def sort(a, anchor, last)
   i = anchor + 1
   j = last
 
-  until i > j
-    # move i up until it a[i] > a[anchor]
-    # move j down until it a[j] < a[anchor]
-      # when both these conditions are met, SWAP
-      # break if EVER i > j
-
-    until a[i] > a[anchor] || i > j || i == last
+  loop do
+    until a[i] > a[anchor] || i == last
       i += 1
-      # p "i: #{i}"
     end
 
-    until a[j] < a[anchor] || i > j || j == anchor
+    until a[j] < a[anchor] || j == anchor
       j -= 1
-      # p "i: #{j}"
     end
 
-    # p "Swapping i #{i} and j #{j} from arr: #{a}"
+    break if i >= j
     a[i], a[j] = a[j], a[i]
-    # p "Result arr #{a}"
   end
 
-  a[i], a[anchor] = a[anchor], a[i]
-  "Final swap #{a}"
+  # move anchor to pivot point
+    # At this point, i will be greater or equal, j will be less than or equal
+  a[j], a[anchor] = a[anchor], a[j]
 
-  unless last - anchor == 1
+  # Do this again
+  if last - anchor > 1
     sort a, anchor, j
-    sort a, j, last
+    sort a, i, last
   end
 end
 
+# TEST
 quick_sort [1]
 quick_sort [3, 1]
 quick_sort [1, 3]
