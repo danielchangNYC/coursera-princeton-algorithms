48895aafe64fd7e0cffa6624388ea5821533f07a
Daniel Chang <daniel.chang85@gmail.com>
2017-03-03 01:16:45 -0500

    Merge sort

--- week-3/merge_sort.rb
+++ week-3/merge_sort.rb
@@ -1,30 +1,54 @@
-def merge(arr)
-  # PRECONDITION: halves already sorted
-  lo = 0
-  hi = arr.length - 1
-  mid = hi / 2
+=begin
+  COST: nlogn
+    - comparison is linear
+    - dividing up the tree is logarithmic
+
+  VARIANT: Bottom Up Merge Sort
+    - STRATEGY Sort pairs, then merge to 4s, 8s, 16s... until sorted
+    - CON: Takes up more space
+=end
+
+def merge_sort(arr)
+  return if arr.length < 2
+  orig_arr_ref = arr
+
+  sort(arr, 0, arr.length - 1)
+  arr
+end
+
+def sort(arr, lo, hi)
+  return if hi <= lo
+  mid = (lo + hi) / 2
+
+  # recursively, sort each half
+  sort(arr, lo, mid)
+  sort(arr, mid + 1, hi)
+  merge(arr, lo, hi, mid)
+end
+
+def merge(arr, lo, hi, mid)
+  return if hi <= lo
+  orig_arr_ref = arr.dup
 
   left_pointer = lo
   right_pointer = mid + 1
 
-  arr.inject([]) do |acc, _|
+  (lo..hi).each do |current_idx|
     if left_pointer > mid
-      acc.push arr[right_pointer]
+      arr[current_idx] = orig_arr_ref[right_pointer]
       right_pointer += 1
     elsif right_pointer > hi
-      acc.push arr[left_pointer]
+      arr[current_idx] = orig_arr_ref[left_pointer]
       left_pointer += 1
-    elsif arr[left_pointer] < arr[right_pointer]
-      acc.push arr[left_pointer]
+    elsif orig_arr_ref[left_pointer] < orig_arr_ref[right_pointer]
+      arr[current_idx] = orig_arr_ref[left_pointer]
       left_pointer += 1
     else
-      acc.push arr[right_pointer]
+      arr[current_idx] = orig_arr_ref[right_pointer]
       right_pointer += 1
     end
-
-    acc
   end
 end
 
-merge([3, 5, 4, 9])
-merge([3, 5, 5, 7, 9, 1, 5, 8, 9])
+merge_sort([3, 5, 4, 9])
+merge_sort([3, 5, 5, 7, 9, 1, 5, 8, 9])
--- week-3/notes.md
+++ week-3/notes.md
@@ -8,4 +8,27 @@
 - Recursively (merge)sort each half
 - Merge two halves
 
+In my own words...
+
+To merge sort:
+  - Recursively for each range of indices
+    - sort the left half
+    - sort the right half
+    - merge from lo to hi
+
+    - Sorting:
+      - return if the lo and hi indices are the same
+      - Divide into halves again
+      - sort left
+      - sort right
+      - merges from lo to hi
+
+    - Merging:
+      - divide into halves
+      - set left and right-side pointers
+      - from lo to hi
+        - compare pointers
+        - if either pointer passes the end of their half, automatically take the other side's value for array[current_index]
+        - set array[current_index] to lower one. Increment that pointer.
+
 ## Quick Sort
