988c1d1d552b01e0c030df46115fd61c723325b2
Daniel Chang <daniel.chang85@gmail.com>
2017-03-04 18:17:23 -0500

    Heap sort

--- week-4/heapsort.rb
+++ week-4/heapsort.rb
@@ -19,9 +19,9 @@ class Heap
     self.heap = heap.unshift(nil)
   end
 
-  def sort
+  def heap_sort
     make_max_heap
-    # sort
+    sort
   end
 
   def make_max_heap
@@ -31,13 +31,28 @@ class Heap
       sink k, heap.length - 1
       k -= 1
     end
+
+    heap
+  end
+
+  def sort
+    # PRINCIPLE: Root is always largest
+    last_pointer = heap.length - 1
+
+    until last_pointer <= 1
+      heap[1], heap[last_pointer] = heap[last_pointer], heap[1]
+      last_pointer -= 1
+      sink 1, last_pointer
+    end
+
+    heap.slice 1..-1
   end
 
   def sink(lo, hi)
     parent = lo
     child = parent * 2
 
-    until child >= hi
+    until child > hi
       if child + 1 <= hi && heap[child + 1] > heap[child]
         child += 1
       end
@@ -52,6 +67,9 @@ end
 
 # Test
 
+op = Heap.new([0, 1])
+op.make_max_heap
+
 h = Heap.new([3, 9, 2, 18, 29, 30, 29, 31, 1, 0, 2])
-h.make_max_heap
-h.sort # [0, 1, 2, 2, 3, 9, 18, 29, 29, 30, 31]
+# h.make_max_heap [nil, 31, 30, 29, 29, 18, 9, 3, 2, 1, 0, 2]
+h.heap_sort # [0, 1, 2, 2, 3, 9, 18, 29, 29, 30, 31]
