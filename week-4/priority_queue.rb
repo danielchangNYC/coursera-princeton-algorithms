32379dbfadcc1f67ddbca0054c3d9b2e166668b8
Daniel Chang <daniel.chang85@gmail.com>
2017-03-04 16:04:07 -0500

    Add cost

--- week-4/priority_queue.rb
+++ week-4/priority_queue.rb
@@ -14,12 +14,14 @@
     2. Ordered array: sort on insert.
       - COST: Insert N, Delete 1
     3. Binary Heap
-      - a[0] empty. a[1] is the root.
-      - Use array indices to represent the tree
-        - parent of node at k is at k/2
-        - children of node are at 2k and 2k+1
-      - PROMOTION: Recursively swap with parent until the heap condition is satisfied
-      - DEMOTION: Check children, switch with the one that's larger. Repeat
+      - COST: Insert LogN, Delete LogN
+      - STRATEGY: Heap
+        - a[0] empty. a[1] is the root.
+        - Use array indices to represent the tree
+          - parent of node at k is at k/2
+          - children of node are at 2k and 2k+1
+        - PROMOTION: Recursively swap with parent until the heap condition is satisfied
+        - DEMOTION: Check children, switch with the one that's larger. Repeat
 =end
 
 class PriorityQueue
